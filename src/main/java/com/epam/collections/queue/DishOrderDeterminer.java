fa8edad7f4b442a46f2cb23c81c52b56436fe44b
Laziz <lazizo2004@gmail.com>
2023-07-08 19:21:16 +0500

    Done 1.0

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,27 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+import java.util.*;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= numberOfDishes; i++) {
+            queue.add(i);
+        }
+
+        List<Integer> dishOrder = new ArrayList<>();
+        int count = 1;
+
+        while (!queue.isEmpty()) {
+            int dishNumber = queue.poll();
+            if (count % everyDishNumberToEat == 0) {
+                dishOrder.add(dishNumber);
+            } else {
+                queue.add(dishNumber);
+            }
+            count++;
+        }
+
+        return dishOrder;
     }
 }
